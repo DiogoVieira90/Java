3e218306e977597edf1645c7dfd4f0ca17442c27
Diogo <diogo.vieira13@etec.sp.gov.br>
2019-10-24 20:20:35 -0300

    Projeto java

---
+++ src/br/com/main/Calcular.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.main;
+
+/**
+ * @author etecja
+ *
+ */
+public class Calcular {
+	
+	public double calcularImc(double peso, double altura) {
+		
+		return peso / (altura * altura);
+	}
+}
---
+++ src/br/com/main/Principal.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package br.com.main;
+
+import java.util.Scanner;
+
+/**
+ * @author etecja
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		double peso;
+		double altura;
+		
+		
+		System.out.println("Qual o seu peso?");
+		peso = scanner.nextDouble();
+		
+		System.out.println("Qual a sua altura?");
+		altura = scanner.nextDouble();
+		
+		Calcular calcular = new Calcular();
+		System.out.println("Resultado: " + calcular.calcularImc(peso, altura));
+	
+	
+	}
+
+}
